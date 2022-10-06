804b60bb5e8023191fbac9406c8054c8931681d7
L. E. Segovia <amy@amyspark.me>
2022-10-06 13:15:57 +0000

    meson: Allow the component options to be updateable
    To trigger the update, run find_things.py and find_things_extern.py
    with the new --meson-options flag.
    
    The README.meson file has been updated accordingly.

--- find_things.py
+++ find_things.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # Copyright (c) 2018 Mathieu Duponchelle <mathieu@centricular.com>
+# Copyright (c) 2022 L. E. Segovia <amy@amyspark.me>
 #
 # This file is part of the FFmpeg Meson build
 #
@@ -18,16 +19,57 @@
 # License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
 import argparse
-import os
 import re
-import sys
+
+def list_components(infile, full):
+    things = []
+
+    for line in infile:
+        if not full:
+            matches = re.match(
+                r'^[^#]*extern AVFilter\s*ff_[^_]*_(.*);', line.strip())
+        else:
+            matches = re.match(r'^[^#]*extern AVFilter\s*ff_(.*_.*);', line.strip())
+        if (matches):
+            if not args.full:
+                things.append(('%s_filter' % matches.group(1).strip()))
+            else:
+                things.append(matches.group(1).strip())
+
+    return things
+
+def update_meson_options(options):
+    has_generated = False
+    lines = []
+    with open('meson_options.txt', 'r', encoding='utf8') as meson_file:
+        opening = '#### --- GENERATED FILTER OPTIONS --- ####\n'
+        closing = opening.replace('GENERATED', 'END GENERATED')
+        for l in meson_file.readlines():
+            if l == opening:
+                has_generated = True
+                lines.append(l)
+                for option in options:
+                    if re.match(f'^null(.*)_filter', option):
+                        value = 'enabled'
+                    else:
+                        value = 'auto'
+                    lines.append(
+                        ('option(\'%s\', type: \'feature\', value: \'%s\')\n' % (option, value)))
+            elif l == closing:
+                lines.append(l)
+                has_generated = False
+            elif not has_generated:
+                lines.append(l)
+
+    with open('meson_options.txt', 'w') as meson_file:
+        meson_file.write(''.join(lines))
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description='Find filter registrations in allfilters.c')
 
+    arg_parser.add_argument('--update-options', action='store_true',
+                            help='update the Meson options list and exit')
     arg_parser.add_argument('--full', action='store_true')
-    arg_parser.add_argument('--meson-options', action='store_true',
-                            help='format list into Meson options')
     arg_parser.add_argument('files', metavar='FILE', nargs='*',
                             type=argparse.FileType('r'),
                             help='File with lists of declarations, ' +
@@ -37,17 +79,17 @@ if __name__ == '__main__':
 
     things = []
 
-    for infile in args.files:
-        for line in infile:
-            if not args.full:
-                matches = re.match(r'^[^#]*extern AVFilter\s*ff_[^_]*_(.*);', line.strip())
-            else:
-                matches = re.match(r'^[^#]*extern AVFilter\s*ff_(.*_.*);', line.strip())
-            if (matches):
-                if args.meson_options:
-                    print('option(\'%s_filter\', type: \'feature\', value: \'auto\')' %
-                          matches.group(1).strip())
-                elif not args.full:
-                    print ('%s_filter' % matches.group(1).strip())
-                else:
-                    print (matches.group(1).strip())
+    if args.update_options:
+        options = {}
+        with open('libavfilter/allfilters.c', 'r', encoding='utf-8') as infile:
+                components = list_components(
+                    infile, False)
+                options = components
+
+        update_meson_options(options)
+    else:
+        things = []
+        for infile in args.files:
+            things += list_components(infile, args.full)
+
+        print('\n'.join(things))
--- find_things_extern.py
+++ find_things_extern.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # Copyright (c) 2018 Mathieu Duponchelle <mathieu@centricular.com>
+# Copyright (c) 2022 L. E. Segovia <amy@amyspark.me>
 #
 # This file is part of the FFmpeg Meson build
 #
@@ -18,9 +19,60 @@
 # License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
 import argparse
-import os
 import re
-import sys
+
+EXTERN_THINGS = [
+    ['AVOutputFormat', 'muxer', 'libavformat/allformats.c', 'muxer_list'],
+    ['AVInputFormat', 'demuxer', 'libavformat/allformats.c', 'demuxer_list'],
+    ['AVCodec', 'encoder', 'libavcodec/allcodecs.c', 'encoder_list'],
+    ['AVCodec', 'decoder', 'libavcodec/allcodecs.c', 'decoder_list'],
+    ['AVCodecParser', 'parser', 'libavcodec/parsers.c', 'parser_list'],
+    ['AVBitStreamFilter', 'bsf', 'libavcodec/bitstream_filters.c', 'bsf_list'],
+    ['AVHWAccel', 'hwaccel', 'libavcodec/hwaccels.h', 'hwaccel_list'],
+    ['URLProtocol', 'protocol', 'libavformat/protocols.c', 'protocol_list'],
+    ['AVOutputFormat', 'muxer', 'libavdevice/alldevices.c', 'outdev_list', 'outdev'],
+    ['AVInputFormat', 'demuxer', 'libavdevice/alldevices.c', 'indev_list', 'indev'],
+]
+
+def list_components(infile, type, suffix, thing_suffix):
+    things = []
+
+    for line in infile:
+        matches = re.match(r'^extern.*' + type +
+                            ' *ff_([^ ]*)_' + suffix + ';', line.strip())
+        if matches:
+            things += [matches.group(1).strip()]
+
+    return ['%s_%s' % (thing, thing_suffix) for thing in things]
+
+def update_meson_options(options):
+    has_generated = False
+    lines = []
+    with open('meson_options.txt', 'r', encoding='utf8') as meson_file:
+        opening = '#### --- GENERATED EXTERN OPTIONS --- ####\n'
+        closing = opening.replace('GENERATED', 'END GENERATED')
+        for l in meson_file.readlines():
+            if l == opening:
+                has_generated = True
+                lines.append(l)
+                for component, list in options.items():
+                    lines.append(f'# Generated {component} options\n')
+                    for option in list:
+                        if re.match(f'^null_{component}', option):
+                            value = 'enabled'
+                        else:
+                            value = 'auto'
+                        lines.append(
+                            ('option(\'%s\', type: \'feature\', value: \'%s\')\n' % (option, value)))
+                    lines.append('\n')
+            elif l == closing:
+                lines.append(l)
+                has_generated = False
+            elif not has_generated:
+                lines.append(l)
+
+    with open('meson_options.txt', 'w') as meson_file:
+        meson_file.write(''.join(lines))
 
 # Used to extract list of muxers, demuxers, encoders, etc.
 if __name__ == '__main__':
@@ -29,14 +81,14 @@ if __name__ == '__main__':
     arg_parser.add_argument('--suffix', metavar='STRING',
                             default='',
                             help='Variable name suffix, e.g. muxer')
+    arg_parser.add_argument('--update-options', action='store_true',
+                            help='update the Meson options list and exit')
     arg_parser.add_argument('--type', metavar='STRING',
                             default='',
                             help='Variable type, e.g. AVOutputFormat')
     arg_parser.add_argument('--thing-suffix', metavar='STRING',
                             default='',
                             help='suffix to add to the thing name')
-    arg_parser.add_argument('--meson-options', action='store_true',
-                            help='format list into Meson options')
     arg_parser.add_argument('files', metavar='FILE', nargs='*',
                             type=argparse.FileType('r'),
                             help='File with lists of declarations, ' +
@@ -44,17 +96,28 @@ if __name__ == '__main__':
 
     args = arg_parser.parse_args()
 
-    things = []
+    if args.update_options:
+        options = {}
 
-    for infile in args.files:
-        for line in infile:
-            matches = re.match(r'^extern.*' + args.type + ' *ff_([^ ]*)_' + args.suffix + ';', line.strip())
-            if matches:
-                things += [matches.group(1).strip()]
+        for extern_thing in EXTERN_THINGS:
+            type = extern_thing[0]
+            suffix = extern_thing[1]
+            filename = extern_thing[2]
 
-    for thing in things:
-        option = '%s_%s' % (thing, args.thing_suffix)
-        if args.meson_options:
-            print('option(\'%s\', type: \'feature\', value: \'auto\')' % option)
-        else:
-            print(option)
+            if len(extern_thing) > 4:
+                thing_suffix = extern_thing[4]
+            else:
+                thing_suffix = suffix
+
+            with open(filename, 'r', encoding='utf-8') as infile:
+                components = list_components(
+                    infile, type, suffix, thing_suffix)
+                options[thing_suffix] = components
+
+        update_meson_options(options)
+    else:
+        things = []
+        for infile in args.files:
+            things += list_components(infile, args.type, args.suffix, args.thing_suffix)
+
+        print('\n'.join(things))
